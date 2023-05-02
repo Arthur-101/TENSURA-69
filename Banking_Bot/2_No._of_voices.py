65ba7230ef3074836d2199012698c6c62efb4a9e
Saurav Arthur <120392872+Arthur-101@users.noreply.github.com>
2023-05-02 21:24:27 +0530

    Delete Banking_Bot directory

--- Banking_Bot/2_No._of_voices.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-for voice in voices:
-    engine.setProperty('voice',voice.id)
-    engine.say("Hello World! Hello. World! Hello, World!")
-    engine.runAndWait()
-    print(voice.name)
-engine.stop()
